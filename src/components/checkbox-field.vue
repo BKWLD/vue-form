18413bfa67511da90b0eb492a548dcc3469bebf2
Jonathan Jahr <jonjahr@gmail.com>
2022-05-06 16:31:17 -0700

    Update checkmark icon.  Add .checked class for styling

--- src/components/checkbox-field.vue
+++ src/components/checkbox-field.vue
@@ -19,9 +19,10 @@
 		)
 		//- Visual checkbox
 		.checkbox-bkg
-			span.checkbox-icon
-				svg(viewBox="0 0 44.27 33.51" xmlns="http://www.w3.org/2000/svg"): path(d="m12.24 33.51a2.25 2.25 0 0 1 -1.58-.64l-10-9.82a2.25 2.25 0 0 1 3.16-3.21l8.41 8.27 28.22-27.47a2.25 2.25 0 1 1 3.14 3.22l-29.78 29a2.24 2.24 0 0 1 -1.57.65z")
-
+			.checkbox-icon
+				//- Slot so consumers can customize the checkbox icon
+				slot(name='icon')
+					svg(height="13" viewBox="0 0 15 13" width="15" xmlns="http://www.w3.org/2000/svg"): path(d="m14.0490929.39756505c.6352385.57197132.686547 1.55066142.1145591 2.18591833l-2.348821 2.61074598c-.055197.09843357-.1220866.1923209-.2007714.27970918l-2.26971236 2.52306152c-.04289025.06539265-.09129155.12835983-.14523693.18827224l-2.62789908 2.9185776c-.04138678.0459647-.08490312.088872-.1302648.128707-.07136821.13653-.16447935.2651094-.27946506.3800951-.60443327.6044333-1.5844849.6044333-2.18891817 0l-2.77704418-2.77704419-.09651902-.10660781-.64566848-.64568498c-.60445078-.60445079-.60443327-1.5844849 0-2.18891817s1.58446738-.60445078 2.18891817 0l2.34275031 2.34260315 1.55716536-1.73018471c.04246315-.0644574.09029183-.12654503.1435177-.18565835l2.34882606-2.61075163c.05519586-.09843229.12208385-.19231778.20076633-.27970353l2.62789905-2.91857761c.5719879-.63525691 1.5506799-.68653044 2.1859184-.11455912z" fill-rule="evenodd" transform="translate(.098785 .173234)")
 		//- Label
 		.label {{ label }}
 
@@ -40,6 +41,8 @@
 import isField from '../concerns/is-field'
 import hasValidation from '../concerns/has-validation'
 import TooltipBtn from '../components-private/tooltip-btn.vue'
+
+
 
 export default
 	mixins: [ isField, hasValidation ]
@@ -62,6 +65,7 @@ export default
 	computed:
 		classes: -> [
 			'inverted' if @inverted
+			if @value then 'checked' else 'unchecked'
 			...@commonClasses
 		]
 
